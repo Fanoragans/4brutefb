aa9e92b7b2992f1aeba5d58973d7251f5355df61
Fanora ID <106912379+Fanoragans@users.noreply.github.com>
2022-06-15 09:36:31 +0700

    Update and rename README.md to 4brutefb.py

---
+++ 4brutefb.py
@@ -0,0 +1,22 @@
+```bash
+✅✅✅✅👉PAID👈✅✅✅✅
+ pkg install git
+
+ pkg install python
+
+ pkg  install python2
+
+ pip2 install requests
+
+ pip2 install requirements
+
+ pip2 install mechanize
+
+ pip2 install requests bs4
+
+git clone https://github.com/Fanoragans/4brutefb
+
+cd 4brutefb
+
+python 4brutefbenc.py
+```
